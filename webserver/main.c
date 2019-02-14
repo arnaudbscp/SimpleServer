089ec7a1a866aa4c7ea410c5c8f7f9919b77073c
Axelle CLARISSE <axelle.clarisse@etudiant.univ-lille1.fr>
2019-02-14 10:29:19 +0100

    code lisible

--- webserver/main.c
+++ webserver/main.c
@@ -12,7 +12,7 @@
 
 #define BUFFER_SIZE 8000
 
-void signal_handler (int sig) { 
+void traitement_signal (int sig) { 
 	printf("Signal %d reçu \n", sig);
 	while(1){
 		if(waitpid(-1, NULL, WNOHANG)<=0){
@@ -25,18 +25,17 @@ void initialiser_signaux (void) {
 	if(signal(SIGPIPE,SIG_IGN)==SIG_ERR){
 		perror ("signal");
 	}
-	struct sigaction s;
-	s.sa_handler= signal_handler;
-	sigemptyset(&s.sa_mask);
-	s.sa_flags = SA_RESTART;
+	struct sigaction sa;
+	sa.sa_handler = traitement_signal;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
 	
-	if (sigaction(SIGCHLD, &s, NULL) == -1) {
+	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
 		perror ("sigaction(SIGCHLD)");
 	}
 }
 
-int main (void)
-{
+int main (void) {
 	/* Arnold Robbins in the LJ of February ’95 , describing RCS 
 	if ( argc > 1 && strcmp ( argv [1] , " -advice " ) == 0) {
 	printf ( " Don ’t Panic !\n " );
@@ -48,50 +47,35 @@ int main (void)
 	int socket_client;
 	int socket_serveur = creer_serveur(8080);
 	initialiser_signaux();
-
-	/*while(1) {
+		
+	while(1) {
 		socket_client = accept(socket_serveur , NULL, NULL);
 		if (socket_client == -1){
 			perror ("accept");
 			//traitement d ’ erreur
 		}
-		const char * message_bienvenue = "Bonjour, \nNous vous souhaitons la bienvenue sur notre serveur ! \nC'est un immense plaisir de vous voir ici. \nNous espérons que vous serez satisfait \net que tout se passera pour le mieux. \nEn attendant, \nnous vous souhaitons un agréable moment. \nSi vous rencontrez un quelconque problème \nn'hésitez pas à nous contacter.\nNous restons à votre entière disposition. \n";
-		
-		int i;
-		for (i=0; i<10; i++) {	
-			write(socket_client,message_bienvenue,strlen(message_bienvenue));
-			sleep(1);
-		}
-		char buffer[BUFFER_SIZE];
-		FILE* file = fopen("text.txt", "r");
-		//write(socket_client, fgets(buff, 255, file), 100000);
-		//fclose(file);
-		
-		while (fgets(buffer, BUFFER_SIZE, file) != NULL) {
-			write(socket_client, fgets(buffer, BUFFER_SIZE, file), BUFFER_SIZE);
-		}
-		fclose(file);*/
-		
-		
-		
- 		while(1) {
-			socket_client = accept(socket_serveur , NULL, NULL);
-			int pid = fork();
-			if (pid == 0) {
-				if (socket_client == -1){
-					perror ("accept");
-					//traitement d ’ erreur
-				}
 
-				const char * message_bienvenue = "Bonjour, \nNous vous souhaitons la bienvenue sur notre serveur ! \nC'est un immense plaisir de vous voir ici. \nNous espérons que vous serez satisfait \net que tout se passera pour le mieux. \nEn attendant, \nnous vous souhaitons un agréable moment. \nSi vous rencontrez un quelconque problème \nn'hésitez pas à nous contacter.\nNous restons à votre entière disposition. \n";
-		
-				int i;
-				for (i=0; i<10; i++) {	
-					write(socket_client,message_bienvenue,strlen(message_bienvenue));
-					sleep(1);
-				}
-			}else {
-				close(socket_client);		
+		const char * message_bienvenue = "Bonjour, \nNous vous souhaitons la bienvenue sur notre serveur ! \nC'est un immense plaisir de vous voir ici. \nNous espérons que vous serez satisfait \net que tout se passera pour le mieux. \nEn attendant, \nnous vous souhaitons un agréable moment. \nSi vous rencontrez un quelconque problème \nn'hésitez pas à nous contacter.\nNous restons à votre entière disposition. \n";
+
+		int pid = fork();
+		if (pid == 0) {
+			int i;
+			for (i=0; i<10; i++) {	
+				write(socket_client,message_bienvenue,strlen(message_bienvenue));
+				sleep(1);
 			}
+
+			/*char buffer[BUFFER_SIZE];
+			FILE* file = fopen("text.txt", "r");
+			write(socket_client, fgets(buff, 255, file), 100000);
+			fclose(file);
+			
+			while (fgets(buffer, BUFFER_SIZE, file) != NULL) {
+				write(socket_client, fgets(buffer, BUFFER_SIZE, file), BUFFER_SIZE);
+			}
+			fclose(file);*/
+		} else {
+			close(socket_client);		
 		}
+	}
 }
